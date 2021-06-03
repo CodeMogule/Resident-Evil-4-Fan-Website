8ead39e80792f276017de0bcc10c5df6dea5d187
CodeMogule <codemogule@gmail.com>
2021-06-03 18:42:41 +0300

    Resident Evil 4 Fan Website

--- src/JS/app.js
+++ src/JS/app.js
@@ -37,3 +37,54 @@ function slideTurn(x){
     slider[counter - 1].style.display = "block";
     imgSelect[counter - 1].classList.add('active');
 }
+
+
+// nav-mobile-menu
+
+const hamburger = document.querySelector('.hamburger');
+const x = document.querySelector('.x');
+const nav = document.querySelector('.nav-mobile');
+const page = document.querySelector('.page-wrap')
+const mobileMenu = document.querySelector('.mobile-menu')
+const logo = document.querySelector('.logo-mobile')
+const navItems = document.querySelectorAll('.mobile-main-nav li')
+
+
+
+
+function showMenu(){
+        x.style.visibility = 'visible';
+        x.classList.add('close');
+        hamburger.classList.remove('show')
+        hamburger.style.visibility = 'hidden';
+        nav.style.visibility = 'visible';
+        page.style.opacity = '0.4'
+        mobileMenu.classList.add('close')
+        mobileMenu.style.zIndex='1'
+        logo.style.visibility = 'visible'
+        
+}
+
+function closeMenu(){
+        hamburger.style.visibility = 'visible';
+        hamburger.classList.add('show');
+        x.classList.remove('close')
+        x.style.visibility = 'hidden'
+        nav.style.visibility = 'hidden';
+        page.style.opacity = '1'
+        mobileMenu.style.zIndex='0'
+        logo.style.visibility = 'hidden'
+        mobileMenu.classList.remove('close')
+}
+
+navItems.forEach((curr)=>{
+    curr.addEventListener("click",function(){
+        nav.style.visibility = 'hidden';
+        x.style.visibility = "hidden"
+        page.style.opacity = '1'
+        logo.style.visibility = 'hidden'
+        hamburger.style.visibility = 'visible';
+        mobileMenu.style.zIndex='0'
+    })
+
+})
